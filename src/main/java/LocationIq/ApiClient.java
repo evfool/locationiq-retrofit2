4788a26f89afb8161d048bff78225462f8a637a7
Robert Roth <robert.roth.off@gmail.com>
2019-09-16 10:21:04 +0300

    Minor changes here and there

--- src/main/java/LocationIq/ApiClient.java
+++ src/main/java/LocationIq/ApiClient.java
@@ -25,256 +25,256 @@ import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 public class ApiClient {
 
-    private Map<String, Interceptor> apiAuthorizations;
+  private Map<String, Interceptor> apiAuthorizations;
+  private OkHttpClient.Builder okBuilder;
+  private Retrofit.Builder adapterBuilder;
+  private JSON json;
 
-    private OkHttpClient.Builder okBuilder;
+  public ApiClient() {
+    apiAuthorizations = new LinkedHashMap<String, Interceptor>();
+    createDefaultAdapter();
+  }
 
-    private Retrofit.Builder adapterBuilder;
+  public ApiClient(String[] authNames) {
+    this();
+    for(String authName : authNames) {
+      Interceptor auth;
+      if ("key".equals(authName)) {
+        
+        auth = new ApiKeyAuth("query", "key");
+      } else {
+        throw new RuntimeException("auth name \"" + authName + "\" not found in available auth names");
+      }
 
-    private JSON json;
-
-    public ApiClient() {
-        apiAuthorizations = new LinkedHashMap<String, Interceptor>();
-        createDefaultAdapter();
+      addAuthorization(authName, auth);
     }
+  }
 
-    public ApiClient(String[] authNames) {
-        this();
-        for (String authName : authNames) {
-            Interceptor auth;
-            if ("key".equals(authName)) {
+  /**
+   * Basic constructor for single auth name
+   * @param authName Authentication name
+   */
+  public ApiClient(String authName) {
+    this(new String[]{authName});
+  }
 
-                auth = new ApiKeyAuth("query", "key");
-            } else {
-                throw new RuntimeException("auth name \"" + authName + "\" not found in available auth names");
-            }
+  /**
+   * Helper constructor for single api key
+   * @param authName Authentication name
+   * @param apiKey API key
+   */
+  public ApiClient(String authName, String apiKey) {
+    this(authName);
+    this.setApiKey(apiKey);
+  }
 
-            addAuthorization(authName, auth);
-        }
-    }
+  /**
+   * Helper constructor for single basic auth or password oauth2
+   * @param authName Authentication name
+   * @param username Username
+   * @param password Password
+   */
+  public ApiClient(String authName, String username, String password) {
+    this(authName);
+    this.setCredentials(username,  password);
+  }
 
-    /**
-     * Basic constructor for single auth name
-     * @param authName Authentication name
-     */
-    public ApiClient(String authName) {
-        this(new String[] {authName});
-    }
+  public void createDefaultAdapter() {
+    json = new JSON();
+    okBuilder = new OkHttpClient.Builder();
 
-    /**
-     * Helper constructor for single api key
-     * @param authName Authentication name
-     * @param apiKey API key
-     */
-    public ApiClient(String authName, String apiKey) {
-        this(authName);
-        this.setApiKey(apiKey);
-    }
+    String baseUrl = "https://eu1.locationiq.com/v1";
+    if (!baseUrl.endsWith("/"))
+      baseUrl = baseUrl + "/";
 
-    /**
-     * Helper constructor for single basic auth or password oauth2
-     * @param authName Authentication name
-     * @param username Username
-     * @param password Password
-     */
-    public ApiClient(String authName, String username, String password) {
-        this(authName);
-        this.setCredentials(username, password);
-    }
+    adapterBuilder = new Retrofit
+      .Builder()
+      .baseUrl(baseUrl)
+      .addConverterFactory(ScalarsConverterFactory.create())
+      .addConverterFactory(GsonCustomConverterFactory.create(json.getGson()));
+  }
 
-    public void createDefaultAdapter() {
-        json = new JSON();
-        okBuilder = new OkHttpClient.Builder();
+  public <S> S createService(Class<S> serviceClass) {
+    return adapterBuilder
+      .client(okBuilder.build())
+      .build()
+      .create(serviceClass);
+  }
 
-        String baseUrl = "https://eu1.locationiq.com/v1";
-        if (!baseUrl.endsWith("/"))
-            baseUrl = baseUrl + "/";
+  public ApiClient setDateFormat(DateFormat dateFormat) {
+    this.json.setDateFormat(dateFormat);
+    return this;
+  }
 
-        adapterBuilder = new Retrofit.Builder().baseUrl(baseUrl).addConverterFactory(ScalarsConverterFactory.create()).addConverterFactory(GsonCustomConverterFactory.create(json.getGson()));
-    }
+  public ApiClient setSqlDateFormat(DateFormat dateFormat) {
+    this.json.setSqlDateFormat(dateFormat);
+    return this;
+  }
 
-    public <S> S createService(Class<S> serviceClass) {
-        return adapterBuilder.client(okBuilder.build()).build().create(serviceClass);
-    }
+  public ApiClient setOffsetDateTimeFormat(DateTimeFormatter dateFormat) {
+    this.json.setOffsetDateTimeFormat(dateFormat);
+    return this;
+  }
 
-    public ApiClient setDateFormat(DateFormat dateFormat) {
-        this.json.setDateFormat(dateFormat);
+  public ApiClient setLocalDateFormat(DateTimeFormatter dateFormat) {
+    this.json.setLocalDateFormat(dateFormat);
+    return this;
+  }
+
+
+  /**
+   * Helper method to configure the first api key found
+   * @param apiKey API key
+   * @return ApiClient
+   */
+  public ApiClient setApiKey(String apiKey) {
+    for(Interceptor apiAuthorization : apiAuthorizations.values()) {
+      if (apiAuthorization instanceof ApiKeyAuth) {
+        ApiKeyAuth keyAuth = (ApiKeyAuth) apiAuthorization;
+        keyAuth.setApiKey(apiKey);
         return this;
+      }
     }
+    return this;
+  }
 
-    public ApiClient setSqlDateFormat(DateFormat dateFormat) {
-        this.json.setSqlDateFormat(dateFormat);
+  /**
+   * Helper method to set token for the first Http Bearer authentication found.
+   * @param bearerToken Bearer token
+   * @return ApiClient
+   */
+  public ApiClient setBearerToken(String bearerToken) {
+    for (Interceptor apiAuthorization : apiAuthorizations.values()) {
+      if (apiAuthorization instanceof HttpBearerAuth) {
+        ((HttpBearerAuth) apiAuthorization).setBearerToken(bearerToken);
         return this;
+      }
     }
+    return this;
+  }
 
-    public ApiClient setOffsetDateTimeFormat(DateTimeFormatter dateFormat) {
-        this.json.setOffsetDateTimeFormat(dateFormat);
+  /**
+   * Helper method to configure the username/password for basic auth or password oauth
+   * @param username Username
+   * @param password Password
+   * @return ApiClient
+   */
+  public ApiClient setCredentials(String username, String password) {
+    for(Interceptor apiAuthorization : apiAuthorizations.values()) {
+      if (apiAuthorization instanceof HttpBasicAuth) {
+        HttpBasicAuth basicAuth = (HttpBasicAuth) apiAuthorization;
+        basicAuth.setCredentials(username, password);
         return this;
+      }
     }
+    return this;
+  }
 
-    public ApiClient setLocalDateFormat(DateTimeFormatter dateFormat) {
-        this.json.setLocalDateFormat(dateFormat);
-        return this;
-    }
 
-    /**
-     * Helper method to configure the first api key found
-     * @param apiKey API key
-     * @return ApiClient
-     */
-    public ApiClient setApiKey(String apiKey) {
-        for (Interceptor apiAuthorization : apiAuthorizations.values()) {
-            if (apiAuthorization instanceof ApiKeyAuth) {
-                ApiKeyAuth keyAuth = (ApiKeyAuth) apiAuthorization;
-                keyAuth.setApiKey(apiKey);
-                return this;
-            }
-        }
-        return this;
+  /**
+   * Adds an authorization to be used by the client
+   * @param authName Authentication name
+   * @param authorization Authorization interceptor
+   * @return ApiClient
+   */
+  public ApiClient addAuthorization(String authName, Interceptor authorization) {
+    if (apiAuthorizations.containsKey(authName)) {
+      throw new RuntimeException("auth name \"" + authName + "\" already in api authorizations");
     }
+    apiAuthorizations.put(authName, authorization);
+    okBuilder.addInterceptor(authorization);
+    return this;
+  }
 
-    /**
-     * Helper method to set token for the first Http Bearer authentication found.
-     * @param bearerToken Bearer token
-     * @return ApiClient
-     */
-    public ApiClient setBearerToken(String bearerToken) {
-        for (Interceptor apiAuthorization : apiAuthorizations.values()) {
-            if (apiAuthorization instanceof HttpBearerAuth) {
-                ((HttpBearerAuth) apiAuthorization).setBearerToken(bearerToken);
-                return this;
-            }
-        }
-        return this;
-    }
+  public Map<String, Interceptor> getApiAuthorizations() {
+    return apiAuthorizations;
+  }
 
-    /**
-     * Helper method to configure the username/password for basic auth or password oauth
-     * @param username Username
-     * @param password Password
-     * @return ApiClient
-     */
-    public ApiClient setCredentials(String username, String password) {
-        for (Interceptor apiAuthorization : apiAuthorizations.values()) {
-            if (apiAuthorization instanceof HttpBasicAuth) {
-                HttpBasicAuth basicAuth = (HttpBasicAuth) apiAuthorization;
-                basicAuth.setCredentials(username, password);
-                return this;
-            }
-        }
-        return this;
-    }
+  public ApiClient setApiAuthorizations(Map<String, Interceptor> apiAuthorizations) {
+    this.apiAuthorizations = apiAuthorizations;
+    return this;
+  }
 
-    /**
-     * Adds an authorization to be used by the client
-     * @param authName Authentication name
-     * @param authorization Authorization interceptor
-     * @return ApiClient
-     */
-    public ApiClient addAuthorization(String authName, Interceptor authorization) {
-        if (apiAuthorizations.containsKey(authName)) {
-            throw new RuntimeException("auth name \"" + authName + "\" already in api authorizations");
-        }
-        apiAuthorizations.put(authName, authorization);
-        okBuilder.addInterceptor(authorization);
-        return this;
-    }
+  public Retrofit.Builder getAdapterBuilder() {
+    return adapterBuilder;
+  }
 
-    public Map<String, Interceptor> getApiAuthorizations() {
-        return apiAuthorizations;
-    }
+  public ApiClient setAdapterBuilder(Retrofit.Builder adapterBuilder) {
+    this.adapterBuilder = adapterBuilder;
+    return this;
+  }
 
-    public Interceptor getAuthentication(String authName) {
-        return apiAuthorizations.get(authName);
-    }
+  public OkHttpClient.Builder getOkBuilder() {
+    return okBuilder;
+  }
 
-    public ApiClient setApiAuthorizations(Map<String, Interceptor> apiAuthorizations) {
-        this.apiAuthorizations = apiAuthorizations;
-        return this;
+  public void addAuthsToOkBuilder(OkHttpClient.Builder okBuilder) {
+    for(Interceptor apiAuthorization : apiAuthorizations.values()) {
+      okBuilder.addInterceptor(apiAuthorization);
     }
+  }
 
-    public Retrofit.Builder getAdapterBuilder() {
-        return adapterBuilder;
-    }
-
-    public ApiClient setAdapterBuilder(Retrofit.Builder adapterBuilder) {
-        this.adapterBuilder = adapterBuilder;
-        return this;
-    }
-
-    public OkHttpClient.Builder getOkBuilder() {
-        return okBuilder;
-    }
-
-    public void addAuthsToOkBuilder(OkHttpClient.Builder okBuilder) {
-        for (Interceptor apiAuthorization : apiAuthorizations.values()) {
-            okBuilder.addInterceptor(apiAuthorization);
-        }
-    }
-
-    /**
-     * Clones the okBuilder given in parameter, adds the auth interceptors and uses it to configure the
-     * Retrofit
-     * @param okClient An instance of OK HTTP client
-     */
-    public void configureFromOkclient(OkHttpClient okClient) {
-        this.okBuilder = okClient.newBuilder();
-        addAuthsToOkBuilder(this.okBuilder);
-    }
+  /**
+   * Clones the okBuilder given in parameter, adds the auth interceptors and uses it to configure the Retrofit
+   * @param okClient An instance of OK HTTP client
+   */
+  public void configureFromOkclient(OkHttpClient okClient) {
+    this.okBuilder = okClient.newBuilder();
+    addAuthsToOkBuilder(this.okBuilder);
+  }
 }
 
 /**
- * This wrapper is to take care of this case: when the deserialization fails due to JsonParseException and the
+ * This wrapper is to take care of this case:
+ * when the deserialization fails due to JsonParseException and the
  * expected type is String, then just return the body string.
  */
 class GsonResponseBodyConverterToString<T> implements Converter<ResponseBody, T> {
-    private final Gson gson;
+  private final Gson gson;
+  private final Type type;
 
-    private final Type type;
+  GsonResponseBodyConverterToString(Gson gson, Type type) {
+    this.gson = gson;
+    this.type = type;
+  }
 
-    GsonResponseBodyConverterToString(Gson gson, Type type) {
-        this.gson = gson;
-        this.type = type;
+  @Override public T convert(ResponseBody value) throws IOException {
+    String returned = value.string();
+    try {
+      return gson.fromJson(returned, type);
     }
-
-    @SuppressWarnings("unchecked")
-    @Override
-    public T convert(ResponseBody value) throws IOException {
-        String returned = value.string();
-        try {
-            return gson.fromJson(returned, type);
-        } catch (JsonParseException e) {
-            return (T) returned;
-        }
+    catch (JsonParseException e) {
+      return (T) returned;
     }
+  }
 }
 
-class GsonCustomConverterFactory extends Converter.Factory {
-    private final Gson gson;
+class GsonCustomConverterFactory extends Converter.Factory
+{
+  private final Gson gson;
+  private final GsonConverterFactory gsonConverterFactory;
 
-    private final GsonConverterFactory gsonConverterFactory;
+  public static GsonCustomConverterFactory create(Gson gson) {
+    return new GsonCustomConverterFactory(gson);
+  }
 
-    public static GsonCustomConverterFactory create(Gson gson) {
-        return new GsonCustomConverterFactory(gson);
-    }
+  private GsonCustomConverterFactory(Gson gson) {
+    if (gson == null)
+      throw new NullPointerException("gson == null");
+    this.gson = gson;
+    this.gsonConverterFactory = GsonConverterFactory.create(gson);
+  }
 
-    private GsonCustomConverterFactory(Gson gson) {
-        if (gson == null)
-            throw new NullPointerException("gson == null");
-        this.gson = gson;
-        this.gsonConverterFactory = GsonConverterFactory.create(gson);
-    }
+  @Override
+  public Converter<ResponseBody, ?> responseBodyConverter(Type type, Annotation[] annotations, Retrofit retrofit) {
+    if (type.equals(String.class))
+      return new GsonResponseBodyConverterToString<Object>(gson, type);
+    else
+      return gsonConverterFactory.responseBodyConverter(type, annotations, retrofit);
+  }
 
-    @Override
-    public Converter<ResponseBody, ?> responseBodyConverter(Type type, Annotation[] annotations, Retrofit retrofit) {
-        if (type.equals(String.class))
-            return new GsonResponseBodyConverterToString<Object>(gson, type);
-        else
-            return gsonConverterFactory.responseBodyConverter(type, annotations, retrofit);
-    }
-
-    @Override
-    public Converter<?, RequestBody> requestBodyConverter(Type type, Annotation[] parameterAnnotations, Annotation[] methodAnnotations, Retrofit retrofit) {
-        return gsonConverterFactory.requestBodyConverter(type, parameterAnnotations, methodAnnotations, retrofit);
-    }
+  @Override
+  public Converter<?, RequestBody> requestBodyConverter(Type type, Annotation[] parameterAnnotations, Annotation[] methodAnnotations, Retrofit retrofit) {
+    return gsonConverterFactory.requestBodyConverter(type, parameterAnnotations, methodAnnotations, retrofit);
+  }
 }
--- src/main/java/com/locationiq/client/api/SearchApi.java
+++ src/main/java/com/locationiq/client/api/SearchApi.java
@@ -8,50 +8,27 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface SearchApi {
-    /**
-     * Forward Geocoding The Search API allows converting addresses, such as a street address, into geographic
-     * coordinates (latitude and longitude). These coordinates can serve various use-cases, from placing
-     * markers on a map to helping algorithms determine nearby bus stops. This process is also known as
-     * Forward Geocoding.
-     * @param q Address to geocode (required)
-     * @param format Format to geocode. Only JSON supported for SDKs (required)
-     * @param normalizecity For responses with no city value in the address section, the next available
-     *            element in this order - city_district, locality, town, borough, municipality, village,
-     *            hamlet, quarter, neighbourhood - from the address section will be normalized to city.
-     *            Defaults to 1 for SDKs. (required)
-     * @param addressdetails Include a breakdown of the address into elements. Defaults to 0. (optional)
-     * @param viewbox The preferred area to find search results. To restrict results to those within the
-     *            viewbox, use along with the bounded option. Tuple of 4 floats. Any two corner points of the
-     *            box - &#x60;max_lon,max_lat,min_lon,min_lat&#x60; or
-     *            &#x60;min_lon,min_lat,max_lon,max_lat&#x60; - are accepted in any order as long as they span
-     *            a real box. (optional)
-     * @param bounded Restrict the results to only items contained with the viewbox (optional)
-     * @param limit Limit the number of returned results. Default is 10. (optional, default to 10)
-     * @param acceptLanguage Preferred language order for showing search results, overrides the value
-     *            specified in the Accept-Language HTTP header. Defaults to en. To use native language for the
-     *            response when available, use accept-language&#x3D;native (optional)
-     * @param countrycodes Limit search to a list of countries. (optional)
-     * @param namedetails Include a list of alternative names in the results. These may include language
-     *            variants, references, operator and brand. (optional)
-     * @param dedupe Sometimes you have several objects in OSM identifying the same place or object in
-     *            reality. The simplest case is a street being split in many different OSM ways due to
-     *            different characteristics. Nominatim will attempt to detect such duplicates and only return
-     *            one match; this is controlled by the dedupe parameter which defaults to 1. Since the limit
-     *            is, for reasons of efficiency, enforced before and not after de-duplicating, it is possible
-     *            that de-duplicating leaves you with less results than requested. (optional)
-     * @param extratags Include additional information in the result if available, e.g. wikipedia link,
-     *            opening hours. (optional)
-     * @param statecode Adds state or province code when available to the statecode key inside the address
-     *            element. Currently supported for addresses in the USA, Canada and Australia. Defaults to 0
-     *            (optional)
-     * @return Call&lt;List&lt;Location&gt;&gt;
-     */
-    @GET("search.php")
-    Call<List<Location>> search(@retrofit2.http.Query("q") String q, @retrofit2.http.Query("format") String format, @retrofit2.http.Query("normalizecity") Integer normalizecity,
-                                @retrofit2.http.Query("addressdetails") Integer addressdetails, @retrofit2.http.Query("viewbox") String viewbox,
-                                @retrofit2.http.Query("bounded") Integer bounded, @retrofit2.http.Query("limit") Integer limit,
-                                @retrofit2.http.Query("accept-language") String acceptLanguage, @retrofit2.http.Query("countrycodes") String countrycodes,
-                                @retrofit2.http.Query("namedetails") Integer namedetails, @retrofit2.http.Query("dedupe") Integer dedupe,
-                                @retrofit2.http.Query("extratags") Integer extratags, @retrofit2.http.Query("statecode") Integer statecode);
+  /**
+   * Forward Geocoding
+   * The Search API allows converting addresses, such as a street address, into geographic coordinates (latitude and longitude). These coordinates can serve various use-cases, from placing markers on a map to helping algorithms determine nearby bus stops. This process is also known as Forward Geocoding.
+   * @param q Address to geocode (required)
+   * @param format Format to geocode. Only JSON supported for SDKs (required)
+   * @param normalizecity For responses with no city value in the address section, the next available element in this order - city_district, locality, town, borough, municipality, village, hamlet, quarter, neighbourhood - from the address section will be normalized to city. Defaults to 1 for SDKs. (required)
+   * @param addressdetails Include a breakdown of the address into elements. Defaults to 0. (optional)
+   * @param viewbox The preferred area to find search results.  To restrict results to those within the viewbox, use along with the bounded option. Tuple of 4 floats. Any two corner points of the box - &#x60;max_lon,max_lat,min_lon,min_lat&#x60; or &#x60;min_lon,min_lat,max_lon,max_lat&#x60; - are accepted in any order as long as they span a real box.  (optional)
+   * @param bounded Restrict the results to only items contained with the viewbox (optional)
+   * @param limit Limit the number of returned results. Default is 10. (optional, default to 10)
+   * @param acceptLanguage Preferred language order for showing search results, overrides the value specified in the Accept-Language HTTP header. Defaults to en. To use native language for the response when available, use accept-language&#x3D;native (optional)
+   * @param countrycodes Limit search to a list of countries. (optional)
+   * @param namedetails Include a list of alternative names in the results. These may include language variants, references, operator and brand. (optional)
+   * @param dedupe Sometimes you have several objects in OSM identifying the same place or object in reality. The simplest case is a street being split in many different OSM ways due to different characteristics. Nominatim will attempt to detect such duplicates and only return one match; this is controlled by the dedupe parameter which defaults to 1. Since the limit is, for reasons of efficiency, enforced before and not after de-duplicating, it is possible that de-duplicating leaves you with less results than requested. (optional)
+   * @param extratags Include additional information in the result if available, e.g. wikipedia link, opening hours. (optional)
+   * @param statecode Adds state or province code when available to the statecode key inside the address element. Currently supported for addresses in the USA, Canada and Australia. Defaults to 0 (optional)
+   * @return Call&lt;List&lt;Location&gt;&gt;
+   */
+  @GET("search.php")
+  Call<List<Location>> search(
+    @retrofit2.http.Query("q") String q, @retrofit2.http.Query("format") String format, @retrofit2.http.Query("normalizecity") Integer normalizecity, @retrofit2.http.Query("addressdetails") Integer addressdetails, @retrofit2.http.Query("viewbox") String viewbox, @retrofit2.http.Query("bounded") Integer bounded, @retrofit2.http.Query("limit") Integer limit, @retrofit2.http.Query("accept-language") String acceptLanguage, @retrofit2.http.Query("countrycodes") String countrycodes, @retrofit2.http.Query("namedetails") Integer namedetails, @retrofit2.http.Query("dedupe") Integer dedupe, @retrofit2.http.Query("extratags") Integer extratags, @retrofit2.http.Query("statecode") Integer statecode
+  );
 
 }
